7ebbcb364cadd609d3a77d13addf9086a342b1f9
EndFroZen <Ef.chanachol@gmail.com>
2024-11-10 16:34:04 +0700

    update server.mjs Have path link css img

--- server.mjs
+++ server.mjs
@@ -2,17 +2,54 @@
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs/promises';
 import path from "node:path";
-const filename = path.join("app",)
-const server = createServer(async (req, res) => {
-  const html_data = await readFile(`${filename}/page/main.html`)
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end( html_data);
-  const reqPath = decodeURIComponent(req.url || '');
-  console.log(reqPath)
-  if(reqPath.startsWith("/style")){
-    console.log("error 404")
+import { console } from 'node:inspector';
+const filename = path.join("app")
+
+// html path
+const server = createServer(async (req,res)=>{
+  const reqPath = decodeURIComponent(req.url || '')
+  if (reqPath === '/' || reqPath === '/index.html') {
+    try {
+      const html_data = await readFile(path.join(filename, 'page', 'main.html'));
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html_data);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading HTML file');
+    }
+  }else if (reqPath === '/home/about' ) {
+    try {
+      const html_data = await readFile(path.join(filename, 'page', 'aboute.html'));
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html_data);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading HTML file');
+    }
   }
-});
+// css path 
+  else if(reqPath.startsWith("/style")){
+    try {
+      const cssPath = await readFile(path.join(filename,reqPath));
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(cssPath);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading css file');
+    }
+  }
+
+  else if(reqPath.startsWith("/image")){
+    try {
+      const imgPath = await readFile(path.join(filename,reqPath));
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(imgPath);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading css file');
+    }
+  }
+})
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
