86a69c4f84fe67b3ce351465c0eb650c3408cf9b
EndFroZen <Ef.chanachol@gmail.com>
2024-11-10 09:59:39 +0700

    update path

---
+++ path.mjs
@@ -0,0 +1,5 @@
+import path from "node:path";
+
+
+const filename = path.join("/app","/page")
+console.log(filename)
--- server.mjs
+++ server.mjs
@@ -1,10 +1,17 @@
 // server.mjs
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs/promises';
+import path from "node:path";
+const filename = path.join("app",)
 const server = createServer(async (req, res) => {
-  const html_data = await readFile("./home.html")
+  const html_data = await readFile(`${filename}/page/main.html`)
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end( html_data);
+  const reqPath = decodeURIComponent(req.url || '');
+  console.log(reqPath)
+  if(reqPath.startsWith("/style")){
+    console.log("error 404")
+  }
 });
 
 // starts a simple http server locally on port 3000
